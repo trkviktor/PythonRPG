099597145eedd921a823bc81bd1ae796f1936591
trkviktor <iroboteditor@gmail.com>
2020-08-26 02:12:23 +0200

    Added item database and item verification

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from colorama import init
 from colorama import Fore, Style
 init()
 
-#Todo: Item Database-->Validate Item Existence-->Player basics-->???
+#Todo: Player basics-->???
 
 #Error Codes:
 #1 - Couldnt add item for some reason,figure it out 
@@ -12,36 +12,46 @@ init()
 print(Fore.GREEN + "Inventory System 0.0.1 by: github.com/trkviktor\n")
 print(Style.RESET_ALL)
 msg = "slot is full"
-items = ["Sword", "Axe"]
+
+items = [
+        "Sword",
+         "Axe",
+         "Armor"
+        ]
+
 
 
 inventorySlot1 = []
-inventorySlot2 = ["armor"]
+inventorySlot2 = []
 inventorySlot3 = []
-inventorySlot4 = ["axe"]
+inventorySlot4 = []
 inventory = [inventorySlot1, inventorySlot2, inventorySlot3,inventorySlot4]
 
 
 
+
+    
+
+
 def addItem(name, slot):
-        if not inventorySlot1:
-            slot = inventorySlot1
-            slot.append(name)
-        elif not inventorySlot2 :
-            slot = inventorySlot2
-            slot.append(name)
-        elif not inventorySlot3:
-            slot = inventorySlot3
-            slot.append(name)
-        elif not inventorySlot4:
-            slot = inventorySlot4
-            slot.append(name)
-        elif inventory:
-            print("\n" + Fore.RED + "Inventory is full")
-            print(Style.RESET_ALL)
-        else:
-            print("\n" + Fore.RED + "Something went wrong-beep boop error code: 1")
-            print(Style.RESET_ALL)
+    if not inventorySlot1:
+        slot = inventorySlot1
+        slot.append(name)
+    elif not inventorySlot2 :
+        slot = inventorySlot2
+        slot.append(name)
+    elif not inventorySlot3:
+        slot = inventorySlot3
+        slot.append(name)
+    elif not inventorySlot4:
+        slot = inventorySlot4
+        slot.append(name)
+    elif inventory:
+        print("\n" + Fore.RED + "Inventory is full")
+        print(Style.RESET_ALL)
+    else:
+        print("\n" + Fore.RED + "Something went wrong-beep boop error code: 1")
+        print(Style.RESET_ALL)
         
     
 def deleteItem(slot):
@@ -84,26 +94,37 @@ def getInventory():
     print("\n----------\n")
 
 
-while True:
-        print("\nEnter your desired menu option:")
-        menuInput = input("Options: Inventory,Additem,Removeitem,Exit :\n\n")
-        if menuInput == "Inventory":
-            getInventory()
-        elif menuInput == "inventory":
-            getInventory()
-        elif menuInput == "Additem":
-            name = input("Enter your items name:")
-            addItem(name, inventorySlot4)
-        elif menuInput == "additem":
-            name = input("Enter your items name:")
-            addItem(name, inventorySlot4)
-        elif menuInput == "Removeitem":
-            deleteItem(inventorySlot4)
-        elif menuInput == "removeitem":
-            deleteItem(inventorySlot4)
-        elif menuInput == "Exit":
-            break
-        elif menuInput == "exit":
-            break
-        else:
-            continue
+def Menu():
+    while True:
+            print("\nEnter your desired menu option:")
+            menuInput = input("Options: Inventory,Additem,Removeitem,Exit :\n\n")
+            if menuInput == "Inventory":
+                getInventory()
+            elif menuInput == "inventory":
+                getInventory()
+            elif menuInput == "Additem":
+                name = input("Enter your items name:")
+                if name in items:
+                     addItem(name, inventorySlot4)
+                     print("\n\n" + name + " Added")
+                else:
+                     print("\n\nItem does not exist")
+            elif menuInput == "additem":
+                name = input("Enter your items name:")
+                if name in items:
+                     addItem(name, inventorySlot4)
+                     print("\n\n" + name + " Added")
+                else:
+                     print("\n\nItem does not exist")
+            elif menuInput == "Removeitem":
+                deleteItem(inventorySlot4)
+            elif menuInput == "removeitem":
+                deleteItem(inventorySlot4)
+            elif menuInput == "Exit":
+                break
+            elif menuInput == "exit":
+                break
+            else:
+                continue
+
+Menu()
